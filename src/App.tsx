b5d66330209df27ba242c8f54ad4713441ff3267
jchhun-FP <127786821+jchhun-FP@users.noreply.github.com>
2023-07-23 02:16:01 -0700

    feat: wip add theme palette and add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 const router = createBrowserRouter([
   {
@@ -8,8 +9,33 @@ const router = createBrowserRouter([
   }
 ]);
 
+// Define what props.theme will look like
+const theme = {
+  palette: {
+    primary: {
+      violet: '#9e7f66',
+      lightViolet: '#939BF4',
+      darkBlue: '#19202D',
+      midnight: '#121721'
+    },
+    secondary: {
+      white: '#ffffff',
+      lightGrey: '#F4F6F8',
+      gray: '#9DAEC2',
+      darkGrey: '#6E8098'
+    }
+  },
+  typography: {
+    fontFamily: 'Kumbh Sans'
+  }
+};
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />;
+    </ThemeProvider>
+  );
 }
 
 export default React.memo(App);
